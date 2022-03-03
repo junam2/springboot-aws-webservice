4f6d4942c25ec017caf15061fd65693e53f17069
junam2 <speedlin4584@gmail.com>
2022-03-03 23:31:14 +0900

    Initial Commit

---
+++ src/main/java/com/junam/book/springbootawswebservice/SpringbootAwsWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.junam.book.springbootawswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAwsWebserviceApplication.class, args);
+    }
+
+}
